382a44f0f8c8888f6d3550d20aab358a7675f263
Dorje-La <ldorje1995@gmail.com>
2022-08-21 18:35:58 -0700

    script.js

---
+++ JSfundamentals/script.js
@@ -0,0 +1,42 @@
+const interestRate = "16%";
+console.log(interestRate);
+
+const myCountry = "Nepal";
+console.log(myCountry);
+
+document.write('Hello')
+
+const age = 45;
+console.log(typeof age);
+
+const evenNumbers = new Array (2,4,6,8,"Hello",[1,2]);
+console.log(evenNumbers[3]);
+
+const bootcamp ={
+    course: "React",
+    duration: '3 months'
+};
+console.log(bootcamp.duration);
+
+const arrayBootcamp =['React','3 months']
+console.log(arrayBootcamp[0]);
+
+function addTwoNumber(first, second){
+    return first+second;
+}
+
+const firstresult = addTwoNumber(10,20);
+console.log(firstresult);
+
+const secondresult = addTwoNumber(100,200);
+console.log(secondresult);
+
+//number
+//string
+
+//Array
+//Object
+//Number
+
+//Primitive --  Number, string
+//Non-primitive -- Array, Object, Function
