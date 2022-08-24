8671454cc5e4f205b6e8fb939f56eba7565cfc1f
Dorje-La <ldorje1995@gmail.com>
2022-08-23 17:47:45 -0700

    accessing dom element

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,125 +1,31 @@
-/*const interestRate = "16%";
-console.log(interestRate);
+let langauge = 'Python';
+let oldlanguage = langauge;
+langauge = 'JavaScript';
 
-const myCountry = "Nepal";
-console.log(myCountry);
+console.log(langauge);
+console.log(oldlanguage);
 
-document.write('Hello')
-
-const age = 45;
-console.log(typeof age);
-
-const evenNumbers = new Array (2,4,6,8,"Hello",[1,2]);
-console.log(evenNumbers[3]);
-
-const bootcamp ={
-    course: "React",
-    duration: '3 months'
+const development = {
+    frontend: "JavaScript",
+    backend: "Java",
 };
-console.log(bootcamp.duration);
 
-const arrayBootcamp =['React','3 months']
-console.log(arrayBootcamp[0]);
+console.log(development);
 
-function addTwoNumber(first, second){
-    return first+second;
-}
+const newDevelopment = development;
+console.log(newDevelopment);
 
-const firstresult = addTwoNumber(10,20);
-console.log(firstresult);
+newDevelopment.backend ="Node.js";
+console.log(newDevelopment);
+console.log(development);
 
-const secondresult = addTwoNumber(100,200);
-console.log(secondresult);
+// document.querySelector(".root").textContent ="New Hello";
+// document.querySelector("div.root").textContent="modified";
+// console.dir(document);
+// document.bgColor = "red";
 
-let number = 10;
-number = 50;
-console.log(number);
+const heading = document.querySelector("h1");
+// console.dir(heading);
+heading.style.color ="red";
 
-//number
-//string
 
-//Array
-//Object
-//Number
-
-//Primitive --  Number, string
-//Non-primitive -- Array, Object, Function
-
-number = 40;
-
-if(number === 50){
-    console.log('value if fifty');
-}else{
-    console.log("value is not fifty");
-}
-
-//Ternary operator
- const isValueFifty =
- number === 50 ?true :false;
-
- // const output  = condition ? '' : '';
-
-console.log(isValueFifty); */
-
-//Global scope
-//Function scope
-//Block scope
-
-//const
-//let
-//var
-
-//Hoisting -- 
-
-const firstname = "Alex";
-
-function calcAge(birthyear){
-//    const age = 40;
-
-//    if(birthyear > 1980){
-//     var add = function (a,b){
-//         console.log(a+b);
-//     }
-//    }
-//    add(10,20);
-
-const firstname = "David";
-console.log(firstname);
-}
-
-calcAge(1990);
-console.log(firstname);
-
-// if(10){
-//     console.log("I am printing truthy result");
-// }else{
-//     console.log("I am printing falsy result");
-// }
-
-//0,'',false, undefined,null,NaN
-
-// if(true)
-// {
-//     var age = 30;
-//     console.log(firstname);
-// }
-
-// console.log(age);
-
-//Function Declaration
-// function addDec(a,b){
-//     console.log(a+b);
-// }
-// addDec(10,20);
-
-// //Function Expression
-// const addExp = function(a,b){
-//     console.log(a+b);
-// }
-// addDec(10,20);
-
-// // Arrow Funtion
-// const addArr =  (a,b) =>{
-//     console.log(a+b);
-// }
-// addDec(10,20);
