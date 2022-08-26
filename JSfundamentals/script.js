7524a1f6420d6a8df352e700a5ac345d7d5c83f3
Dorje-La <ldorje1995@gmail.com>
2022-08-25 17:54:15 -0700

    script.js

--- JSfundamentals/script.js
+++ JSfundamentals/script.js
@@ -1,61 +1,139 @@
-// for loop
-// for of 
-// for in 
-// while
+// // for loop
+// // for of 
+// // for in 
+// // while
 
-// for each
-// Map
-// Filter 
-// reduce 
+// // for each
+// // Map
+// // Filter 
+// // reduce 
 
-// for(let i=0; i<5; i++){
-//     console.log(arr[i]);
+// // for(let i=0; i<5; i++){
+// //     console.log(arr[i]);
+// // }
+
+// // for(number of arr){
+// //     console.log(number);
+// // }
+ 
+// // function display(a){
+// //     // console.log(a);
+// //     a(10);
+// // }
+
+// // display(function (number){
+// //     console.log(number);
+// // });
+
+// //const arr = [11,12,13,14,15,16,17,18,19,20];
+
+// //console.log(arr);
+
+// //arr.forEach((number)=>{console.log(number)});
+
+// const obj = {
+//     name : "Dorje",
+//     age : 30,
+//     favNumber:[1,2,3,4],
+//     calcAge : function(){
+//         console.log("Runing");
+//         return "Your age";
+//     },
+//     freeHours: {
+//         hourOne : "six-seven",
+//         hourTwo: 2100,
+//         allHours:[1800,1900,2100],
+//     },
+// };
+
+// // const firstHour = obj.freeHours.hourOne;
+// // console.log(firstHour);
+
+// const AvailableHours=obj.freeHours.allHours[0];
+// console.log(AvailableHours);
+
+// //Spread operator [,,,,something],
+// const {age :myage,favNumber, freeHours:{hourOne,allHours:[firstHour],}} = obj;
+// console.log(myage);
+// console.log(favNumber);
+// console.log(firstHour);
+
+// const arr = [1,2,3,40,[1,2]];
+
+// // const[a,b,c,d,e] = arr;
+
+// // console.log(a,b,c,d,e);
+
+// // console.log(...arr); Rest operator
+
+// const arrOne =[1,2,3,4];
+// const arrTwo =[5,6,7,8];
+
+// const arrThree = arrOne.concat(arrTwo);
+
+// console.log(arrThree);
+
+// console.log([...arrOne, ...arrTwo]); // Rest parameter = rest of the other elements
+
+// const[first,...two]= arrOne; // Rest parameter- rest of the array elements
+// console.log(two);
+
+// function displayArray(...a){
+//     console.log(...a);
 // }
 
-// for(number of arr){
-//     console.log(number);
-// }
+// displayArray(1,2,3,[1, 2], "hello");
 
-// function display(a){
-//     // console.log(a);
-//     a(10);
-// }
+//Map method
+const arr = [10,20,30,40];
 
-// display(function (number){
-//     console.log(number);
+let newArr = [];
+console.log(newArr);
+
+// const resultforeach = arr.forEach(element => {
+    
+// });((el,i,array)=>{
+//     newArr.push(el);
+//     // console.log(i);
+//     // console.log(el);
+//     // console.log(array);
+//     return el *2;
 // });
 
-//const arr = [11,12,13,14,15,16,17,18,19,20];
+// console.log(resultforeach);
 
-//console.log(arr);
+const resultmap = arr.map((el,i,array)=>{
+    // newArr.push(el);
+    // console.log(i);
+    // console.log(el);
+    // console.log(array);
+    return el *2;
+});
 
-//arr.forEach((number)=>{console.log(number)});
+ console.log(resultmap);
 
-const obj = {
-    name : "Dorje",
-    age : 30,
-    favNumber:[1,2,3,4],
-    calcAge : function(){
-        console.log("Runing");
-        return "Your age";
-    },
-    freeHours: {
-        hourOne : "six-seven",
-        hourTwo: 2100,
-        allHours:[1800,1900,2100],
-    },
-};
+// console.log(newArr);
 
-const firstHour = obj.freeHours.hourOne;
-console.log(firstHour);
+const add = () => {}
 
-const AvailableHours=obj.freeHours.allHours[0];
-console.log(AvailableHours);
+//filter
+//reduce  
 
-const arr = [1,2,[20,30],4];
+//Filter method
+const filterOutput = arr.filter((el,i,array) => {
+    // console.log(i);
+    // console.log(el);
+    // console.log(array);
+    return el > 20;
+})
 
-// const thirdElemet = arr[2];
-// console.log(thirdElemet);
+console.log(filterOutput);
 
-const [,,[,a]]= arr; //Destruction method
-console.log(addEventListener);
+//Filter method
+const reduceOutput = arr.reduce((acc,currentValue) => {
+    console.log(acc);
+    console.log(currentValue);
+    return acc + currentValue;
+},0);
+
+console.log(reduceOutput);
